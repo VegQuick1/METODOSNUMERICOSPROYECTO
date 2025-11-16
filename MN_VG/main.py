515aafd08e939bc37a369c460082d102f082038b
Gerardo U <64242436+VegQuick1@users.noreply.github.com>
2025-11-16 01:12:35 -0600

    Delete MN_VG directory

--- MN_VG/main.py
+++
@@ -1,8 +0,0 @@
-# main.py
-import tkinter as tk
-from game_app import NumericalMethodsGame
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = NumericalMethodsGame(root)
-    root.mainloop()
